24faa97d009efbdd9db04713e38316477a43a0bb
Sarthak Parashar <sarthaksh72002@gmail.com>
2024-11-08 23:53:36 +0530

    Update Geely_Value_Sense_App.py

--- Geely_Value_Sense_App.py
+++ Geely_Value_Sense_App.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import pickle
-from sklearn.ensemble import GradientBoostingRegressor
+import sklearn
 
 gbm = GradientBoostingRegressor()
 
